ab1dd90dd98fc8f30e2c3660bba56d809702c5fb
NuwanthiYapa <35219619+NuwanthiYapa@users.noreply.github.com>
2018-01-12 11:33:53 +0530

    again

--- AmazonS3Test100117/AmazonS3Test100117-lambda.js
+++ AmazonS3Test100117/AmazonS3Test100117-lambda.js
@@ -3,13 +3,13 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
     
     ddb.put({
-        TableName: 'nuwanthi_test',
+        TableName: 'nuwanthi_table',
         Item: {}
     }, function (err, data) {
         if (err) {
             console.log("Error", err);
         } else {
-            console.log("Event", event);
+            console.log("Data", data);
         }
     });
 
