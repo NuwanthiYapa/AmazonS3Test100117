dc0531ea39ac3fe1c6e3680b89a1ac225f21aca4
NuwanthiYapa <35219619+NuwanthiYapa@users.noreply.github.com>
2018-01-12 14:32:40 +0530

    add a SQS

--- AmazonS3Test100117/AmazonS3Test100117-lambda.js
+++ AmazonS3Test100117/AmazonS3Test100117-lambda.js
@@ -1,17 +1,38 @@
 let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-    
+
     ddb.put({
-        TableName: 'nuwanthi_table',
+        TableName: 'hiruS3Sample123',
         Item: {}
     }, function (err, data) {
         if (err) {
-            console.log("Error", err);
+            //handle error
         } else {
-            console.log("Data", data);
+            //your logic goes here
         }
     });
+
+    sqs.sendMessage({
+        MessageBody: 'your message body should be assigned here',
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/492230085248/nuwanthi_queue1',
+        DelaySeconds: '0',
+        MessageAttributes: {
+            "name": {
+                "DataType": "String",
+                "StringValue": "nu"
+            }
+        }
+    }, function (err, data) {
+        if (err) {
+            console.log("Receive Error", err);
+            // your logic (logging etc) to handle failures, should be here
+        } else {
+            // your logic (logging etc) to handle successful completion, should be here
+        }
+    });
+
 
 
 }
